dff32a851adc5de0cb34dcd726d2d4a5d050cb40
Morpheus.D <morpheus.ding@outlook.com>
2024-07-06 21:57:49 +0800

    Update some plugin.

--- lua/plugins/workspace.lua
+++ lua/plugins/workspace.lua
@@ -33,8 +33,17 @@ return {
     },
   },
 
+  --{
+  --"scrooloose/nerdcommenter",
+  --},
+
+  -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
   {
-    "scrooloose/nerdcommenter",
+    "numToStr/Comment.nvim",
+    opts = {
+      -- add any options here
+    },
+    lazy = false,
   },
 
   -- use mini.starter instead of alpha
@@ -44,6 +53,8 @@ return {
   { import = "lazyvim.plugins.extras.lang.json" },
 
   { import = "lazyvim.plugins.extras.editor.mini-files" },
+
+  { import = "lazyvim.plugins.extras.coding.yanky" },
 
   -----------------------------------------------------------------
 }
