8cd49b728a24f5653b9ea6b88616f75f6a3bf9c3
Thomas <dba@duck.com>
2024-11-02 14:44:24 +0800

    Update setting for font at mac.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,13 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
 if vim.g.nvy == 1 then -- for nvy gui client
   vim.opt.guifont = "UDEV Gothic 35NFLG:h12:Consolas"
   -- can not use Sarasa Nerd Font in nvy GUI.
   -- vim.opt.guifont = "Sarasa Fixed CL Nerd Font SemiB:h12:Consolas"
 elseif vim.fn.has("gui_running") then
   --vim.opt.guifont = "UDEV Gothic 35NFLG:h10:Consolas"
-  vim.opt.guifont = "Sarasa Fixed CL Nerd Font SemiB:h10:Consolas"
+  if vim.fn.has("mac") then
+    vim.opt.guifont = "UDEV Gothic NF:h14"
+  else
+    vim.opt.guifont = "Sarasa Fixed CL Nerd Font SemiB:h10:Consolas"
+  end
 end
 
 -- Change leader key
