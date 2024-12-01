69f17c7db0e0e854a9d23dc608f6b0847b6a8988
Thomas <dba@duck.com>
2024-12-01 20:35:12 +0800

    Update mac's setting.

--- lua/config/neovide_config.lua
+++ lua/config/neovide_config.lua
@@ -20,6 +20,20 @@ if vim.g.neovide then
     vim.o.guifont = "Explex Console NF:h11:Consolas"
   end
 
+  if vim.g.is_mac then
+    ---- ウィンドウの設定
+    -- ウィンドウのぼかし(Macのみ)
+    vim.g.neovide_window_blurred = true
+    -- フローティングブラー
+    vim.g.neovide_floating_blur_amount_x = 2.0
+    vim.g.neovide_floating_blur_amount_y = 2.0
+    -- シャドウ
+    vim.g.neovide_floating_shadow = true
+    vim.g.neovide_floating_z_height = 10
+    vim.g.neovide_light_angle_degrees = 45
+    vim.g.neovide_light_radius = 5
+  end
+
   -- リフレッシュレート
   vim.g.neovide_refresh_rate = 60
   -- フォーカスされていないときのリフレッシュ レート
