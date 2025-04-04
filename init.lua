cd6bc746270a762142727f0c489c75bc18a0bd6c
Morpheus.D <morpheus.ding@outlook.com>
2025-04-04 18:57:26 +0800

    move plugin config to plugin-config folder.

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- other config
 require("config.neovide_config")
+require("plugin-config.lualine-config")
+require("plugin-config.nvim-treesitter")
--- lua/plugins/vimeasyalign.lua
+++ lua/plugins/vimeasyalign.lua
@@ -1,3 +1,10 @@
+-- 🌻 A Vim alignment plugin
+-- " Start interactive EasyAlign in visual mode (e.g. vipga)
+-- xmap ga <Plug>(EasyAlign)
+--
+-- " Start interactive EasyAlign for a motion/text object (e.g. gaip)
+-- nmap ga <Plug>(EasyAlign)
+
 return {
   "junegunn/vim-easy-align",
   keys = { "<Plug>(EasyAlign)" },
