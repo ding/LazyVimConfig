6a17cd92f9fe4cce763eb2cdf0b9ce1b49e22c49
Morpheus.D <morpheus.ding@outlook.com>
2025-04-04 23:00:08 +0800

    追加ctrl-j等窗口切换键

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Go to left window', 
+--   silent = true,
+--   noremap = true,  -- 避免递归映射
+--   override = true  -- 强制覆盖其他映射
+-- })
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Go to left window', silent = true, noremap = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Go to right window', silent = true, noremap = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Go to up window', silent = true, noremap = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Go to down window', silent = true, noremap = true })
